8640a9889da88fe5c88f40d3e6f0b556b2b21449
Yves Reecht <Yves.Reecht@marine.ie>
2018-02-27 09:46:08 +0000

    R-training: Exercices 4 with relative path to data + cleaning data.

--- 5.exercices.corrections/4_data_mining.r
+++ 5.exercices.corrections/4_data_mining.r
@@ -1,38 +1,42 @@
-rm(list = ls())
+rm(list = ls())  ## Clear the objects in memory.
 
-getwd()
+getwd() ## the current working directory.
+
+## ...that can be changed:
 wd <-  'C:/Users/gbal/Desktop/r.training/5.exercices.corrections'
 setwd(wd)
 
 ## example relative path for data: "../2.data/2.salmon/salmon.data.raw.txt"
-# loading files =========================================================================
+## "../" jumps to the parent directory, "../../", second parent, etc.
+
+## loading files =========================================================================
 
 # from folder
 salmon.data <- read.table(file = '../2.data/2.salmon/salmon.data.raw.txt',
                           header = TRUE,
-                          sep = ',', 
+                          sep = ',',
                           dec = '.')
 
-# from the web
+## from the web:
 # web.address <- 'https://github.com/GuillaumeBal/r.training/blob/master/2.data/2.salmon/salmon.data.raw.txt'
 # salmon.data <- read.table(file = web.address,
 #                           header = TRUE,
-#                           sep = ',', 
+#                           sep = ',',
 #                           dec = '.')
 
-# load excel from folder
-#install.packages('openxlsx')
+## load excel from folder
+# install.packages('openxlsx')
 require(openxlsx)
-#library(openxlsx)
-#salmon <- read.xlsx('salmon.data.raw.xlsx')
+# library(openxlsx)
+# salmon <- read.xlsx('../2.data/2.salmon/salmon.data.raw.xlsx')
 
 head(salmon.data)
 dim(salmon.data)
 
-write.table(salmon.data, 
+write.table(salmon.data,
             file = 'salmon.g.txt',
             sep = ',',
-            dec = '.', 
+            dec = '.',
             row.names = FALSE)
 
 
@@ -66,7 +70,7 @@ salmonTidy$weight[idx] / 1000
 
 ## Correct them in the object (modify as you access):
 
-# salmonTidy$weight[which(salmonTidy$weight > 50)] <- 
+# salmonTidy$weight[which(salmonTidy$weight > 50)] <-
 #   salmonTidy$weight[which(salmonTidy$weight > 50)] / 1000
 
 salmonTidy$weight[idx]  <- salmonTidy$weight[idx] / 1000
